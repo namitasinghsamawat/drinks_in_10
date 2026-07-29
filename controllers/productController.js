1c9e9906eb3af53aa929bf2c37616aec951f9ba3
namitasinghsamawat <namitasinghsamawat@gmail.com>
2026-07-29 09:27:49 +0530

    implemented get and update product APIS using id

--- controllers/productController.js
+++ controllers/productController.js
@@ -18,47 +18,86 @@ async function getProducts(req, res) {
 }
 
 async function addProducts(req, res) {
-    try{
-    console.log(req.body);
-    const product = req.body;
+    try {
+        console.log(req.body);
+        const product = req.body;
 
-    if (!product.name) {
-        return res.status(400).send("Product name is required");
-    }
+        if (!product.name) {
+            return res.status(400).send("Product name is required");
+        }
 
-    if (product.price<=0) {
-        return res.status(400).send("Product price must be greater than 0 ");
-    }
+        if (product.price <= 0) {
+            return res.status(400).send("Product price must be greater than 0 ");
+        }
 
-    if (product.stock<0) {
-        return res.status(400).send("Product stock is required");
-    }
+        if (product.stock < 0) {
+            return res.status(400).send("Product stock is required");
+        }
 
-    if (!product.category) {
-        return res.status(400).send("Product category is required");
-    }
+        if (!product.category) {
+            return res.status(400).send("Product category is required");
+        }
 
-    if (!product.brand) {
-        return res.status(400).send("Product brand is required");
-    }
+        if (!product.brand) {
+            return res.status(400).send("Product brand is required");
+        }
 
-    if (!product.volume) {
-        return res.status(400).send("Product volume is required");
-    }
-   const savedProduct = await Product.create(product);
+        if (!product.volume) {
+            return res.status(400).send("Product volume is required");
+        }
+        const savedProduct = await Product.create(product);
 
-    res.status(201).json({
-        message:"product added successfully",
-        product: savedProduct
-});
-    }catch(error)
-    {
+        res.status(201).json({
+            message: "product added successfully",
+            product: savedProduct
+        });
+    } catch (error) {
         res.status(500).json({
-            message:error.message
+            message: error.message
         })
+    }
+}
+
+async function getProductById(req, res) {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).json({
+                message: "product not found"
+            })
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+async function updateProductById(req, res) {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+               returnDocument:"after"
+            }
+        );
+        if (!updatedProduct) {
+            return res.status(404).json({
+                message: "product not found"
+            });
+        }
+        res.status(200).json(updatedProduct);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
     }
 }
 module.exports = {
     getProducts,
     addProducts,
+    getProductById,
+    updateProductById
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProducts, addProducts } = require("../controllers/productController");
+const { getProducts, addProducts , getProductById , updateProductById} = require("../controllers/productController");
 
 router.get("/", getProducts);
 router.post("/", addProducts);
+router.get("/:id",getProductById);
+router.post("/:id",updateProductById);
 
 module.exports = router;
