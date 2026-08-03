76317bbb81951895dbb80ecc8cce7e9d12fafd27
namitasinghsamawat <namitasinghsamawat@gmail.com>
2026-08-03 09:50:10 +0530

    seller registration completed

--- controllers/sellerController.js
+++ controllers/sellerController.js
@@ -1,7 +1,71 @@
-function registerSeller(req,res)
-{
-    res.send("seller registration");
+const Seller = require("../models/Seller");
+const bcrypt = require("bcrypt");
+
+async function registerSeller(req, res) {
+    try {
+        // read data
+        console.log(req.body);
+
+        const {
+            name,
+            email,
+            password,
+            phone,
+            licenseNumber,
+            shopName,
+            address,
+            role
+        } = req.body;
+        // validate fields
+        if (
+            !name ||
+            !email ||
+            !password ||
+            !phone ||
+            !licenseNumber ||
+            !shopName ||
+            !address
+        ) {
+            return res.status(400).json({
+                message: "All fields are required"
+            });
+        }
+
+        //check email 
+        const existingSeller = await Seller.findOne({ email });
+
+        if (existingSeller) {
+            return res.status(409).json({
+                message: "email already registered"
+            });
+        }
+        // hash password
+        const hashPassword = await bcrypt.hash(password,10);
+
+        // create user
+        const seller = await Seller.create({
+            name,
+            email,
+            password: hashPassword,
+            phone,
+            licenseNumber,
+            shopName,
+            address,
+            role
+            });
+        return res.status(201).json({
+        message: "Seller registered successfully",
+        seller
+        });
+       
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
 }
+
 module.exports = {
     registerSeller
 };
---
+++ models/Seller.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const sellerSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: String,
+            required: true
+        },
+        licenseNumber: {
+            type: String,
+            required: true
+        },
+
+        shopName: {
+            type: String,
+            required: true
+        },
+
+        address: {
+            type: String,
+            required: true
+        },
+
+        role: {
+            type: String,
+            required: true
+        }
+
+
+    }
+);
+
+const Seller = mongoose.model("Seller", sellerSchema);
+
+module.exports = Seller;
