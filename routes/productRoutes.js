effc0d9807b84f488793bd015da1c7e2bc959691
namitasinghsamawat <namitasinghsamawat@gmail.com>
2026-08-07 10:08:45 +0530

    Added product API testing and verified POST request flow

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
 
 const { getProducts, addProducts , getProductById , updateProductById , deleteProductById} = require("../controllers/productController");
 
 router.get("/", getProducts);
-router.post("/", addProducts);
+router.post("/add-product", authMiddleware, addProducts);
 router.get("/:id",getProductById);
 router.post("/:id",updateProductById);
 router.delete("/:id",deleteProductById);
