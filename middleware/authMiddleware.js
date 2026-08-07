cada254562665af818b38ce8edd5499fad9224f8
namitasinghsamawat <namitasinghsamawat@gmail.com>
2026-08-07 20:41:06 +0530

    error resloving in middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -21,7 +21,7 @@ const authMiddleware = (req, res, next) => {
     catch (error) {
         return res.status(401).json({
             message: "Invalid or expired token."
-        })
+        });
     }
 
 };
